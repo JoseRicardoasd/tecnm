401e7fcb915daca11a313c4115106f4a9a78c926
AntonioQk <86135476+AntonioQk@users.noreply.github.com>
2022-10-05 10:19:01 -0500

    Update Ciencias Básicas.php
    cambio de responsable

--- usuarios/cargas/Ciencias Básicas.php
+++ usuarios/cargas/Ciencias Básicas.php
@@ -5,13 +5,13 @@ include('../../app/config/config.php');
 $nombre = $_FILES['archivo']['name'];
 $guardado = $_FILES['archivo']['tmp_name'];
 //se captura el id del evento
-$id_act = $_POST['id_actividad'];
+$id_act = $_POST['evento'];
 //captura del numero de control del usuario
 $num_control = $_POST['numero_control'];
 
-$responsable = $_POST['responsable'];
+//$responsable = $_POST['responsable'];
 
-$nombre_alumno = $_POST['nombre_alumno'];
+$apellido = $_POST['ap_paterno'];
 
 //se verifica si existe la carpeta don de se guardará el archivo
 if (!file_exists('Concurso de Ciencias Básicas')) {
@@ -19,13 +19,13 @@ if (!file_exists('Concurso de Ciencias Básicas')) {
   mkdir('Concurso de Ciencias Básicas', 0777, true);
   if (file_exists('Concurso de Ciencias Básicas')) {
     //Se verifica que el archivo que se sube, no exista en la carpeta, si ya existe no se podrá subir otra vez
-    if (file_exists('Concurso de Ciencias Básicas/' . $num_control . '-' . $nombre_alumno . '-' . $id_act . '-' . 'ConcursoCienciasBasicas.pdf')) {
+    if (file_exists('Concurso de Ciencias Básicas/' . $num_control . '-' . $apellido . '-' . $id_act . '.pdf')) {
       echo '<script language="javascript">alert("El archivo ya existe");window.location.href="../agregar-credito-user.php"</script>';
     } else {
       //Una vez que se confirmó que no se ha subido antes el archivo, se guarda el que se está subiendo a la carpeta que le corresponde
-      if (move_uploaded_file($guardado, 'Concurso de Ciencias Básicas/' . $num_control . '-' . $nombre_alumno . '-' . $id_act . '-' . 'ConcursoCienciasBasicas.pdf')) {
+      if (move_uploaded_file($guardado, 'Concurso de Ciencias Básicas/' . $num_control . '-' . $apellido . '-' . $id_act . '.pdf')) {
 
-        $inserta = "INSERT INTO evidencia(numero_control, id_evento, subido, ruta_doc, responsable) VALUES ('$num_control', '$id_act', 1, 'Concurso de Ciencias Básicas/$num_control-$nombre_alumno-$id_act-ConcursoCienciasBasicas.pdf', '$responsable')";
+        $inserta = "INSERT INTO evidencia(numero_control, id_evento, subido, ruta_doc) VALUES ('$num_control', '$id_act', 1, 'Concurso de Ciencias Básicas/$num_control-$apellido-$id_act.pdf')";
         $resultado = mysqli_query($conexion, $inserta);
         if (!$resultado) {
           echo 'Error al insertar archivo';
@@ -40,13 +40,13 @@ if (!file_exists('Concurso de Ciencias Básicas')) {
   }
 } else {
   //Se verifica que el archivo que se sube, no exista en la carpeta, si ya existe no se podrá subir otra vez
-  if (file_exists('Concurso de Ciencias Básicas/' . $num_control . '-' . $nombre_alumno . '-' . $id_act . '-' . 'ConcursoCienciasBasicas.pdf')) {
+  if (file_exists('Concurso de Ciencias Básicas/' . $num_control . '-' . $apellido . '-' . $id_act . '.pdf')) {
     echo '<script language="javascript">alert("El archivo ya existe");window.location.href="../agregar-credito-user.php"</script>';
   } else {
     //Una vez que se confirmó que no se ha subido antes el archivo, se mueve el que se está subiendo a la carpeta que le corresponde
-    if (move_uploaded_file($guardado, 'Concurso de Ciencias Básicas/' . $num_control . '-' . $nombre_alumno . '-' . $id_act . '-' . 'ConcursoCienciasBasicas.pdf')) {
+    if (move_uploaded_file($guardado, 'Concurso de Ciencias Básicas/' . $num_control . '-' . $apellido . '-' . $id_act . '.pdf')) {
 
-      $inserta = "INSERT INTO evidencia(numero_control, id_evento, subido, ruta_doc, responsable) VALUES ('$num_control', '$id_act', 1, 'Concurso de Ciencias Básicas/$num_control-$nombre_alumno-$id_act-ConcursoCienciasBasicas.pdf', '$responsable')";
+      $inserta = "INSERT INTO evidencia(numero_control, id_evento, subido, ruta_doc) VALUES ('$num_control', '$id_act', 1, 'Concurso de Ciencias Básicas/$num_control-$apellido-$id_act.pdf')";
       $resultado = mysqli_query($conexion, $inserta);
       if (!$resultado) {
         echo 'Error al insertar archivo';

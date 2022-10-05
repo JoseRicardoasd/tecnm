86823c4998acef1082612c0b9ab2e676056abebe
AntonioQk <86135476+AntonioQk@users.noreply.github.com>
2022-10-05 10:48:36 -0500

    Update congreso-seminario.php
    cambios del responsable

--- usuarios/cargas/congreso-seminario.php
+++ usuarios/cargas/congreso-seminario.php
@@ -5,23 +5,23 @@ include('../../app/config/config.php');
 $nombre = $_FILES['archivo']['name'];
 $guardado = $_FILES['archivo']['tmp_name'];
 
-$id_act = $_POST['id_actividad'];
+$id_act = $_POST['evento'];
 
 $num_control = $_POST['numero_control'];
 
-$responsable = $_POST['responsable'];
+//$responsable = $_POST['responsable'];
 
-$nombre_alumno = $_POST['nombre_alumno'];
+$apellido = $_POST['ap_paterno'];
 
 if (!file_exists('Congreso Seminario Simponsio')) {
   mkdir('Congreso Seminario Simponsio', 0777, true);
   if (file_exists('Congreso Seminario Simponsio')) {
-    if (file_exists('Congreso Seminario Simponsio/' . $num_control . '-' . $nombre_alumno . '-' . $id_act . '-' . 'CongresoSeminarioSimponsio.pdf')) {
+    if (file_exists('Congreso Seminario Simponsio/' . $num_control . '-' . $apellido . '-' . $id_act . '.pdf')) {
       echo '<script language="javascript">alert("El archivo ya existe");window.location.href="../agregar-credito-user.php"</script>';
     } else {
-      if (move_uploaded_file($guardado, 'Congreso Seminario Simponsio/' . $num_control . '-' . $nombre_alumno . '-' . $id_act . '-' . 'CongresoSeminarioSimponsio.pdf')) {
+      if (move_uploaded_file($guardado, 'Congreso Seminario Simponsio/' . $num_control . '-' . $apellido . '-' . $id_act . '.pdf')) {
 
-        $inserta = "INSERT INTO evidencia(numero_control, id_evento, subido, ruta_doc, responsable) VALUES ('$num_control', '$id_act', 1, 'Congreso Seminario Simponsio/$num_control-$nombre_alumno-$id_act-CongresoSeminarioSimponsio.pdf', '$responsable')";
+        $inserta = "INSERT INTO evidencia(numero_control, id_evento, subido, ruta_doc) VALUES ('$num_control', '$id_act', 1, 'Congreso Seminario Simponsio/$num_control-$apellido-$id_act.pdf')";
         $resultado = mysqli_query($conexion, $inserta);
         if (!$resultado) {
           echo 'Error al insertar archivo';
@@ -35,12 +35,12 @@ if (!file_exists('Congreso Seminario Simponsio')) {
     }
   }
 } else {
-  if (file_exists('Congreso Seminario Simponsio/' . $num_control . '-' . $nombre_alumno . '-' . $id_act . '-' . 'CongresoSeminarioSimponsio.pdf')) {
+  if (file_exists('Congreso Seminario Simponsio/' . $num_control . '-' . $apellido . '-' . $id_act . '.pdf')) {
     echo '<script language="javascript">alert("El archivo ya existe");window.location.href="../agregar-credito-user.php"</script>';
   } else {
-    if (move_uploaded_file($guardado, 'Congreso Seminario Simponsio/' . $num_control . '-' . $nombre_alumno . '-' . $id_act . '-' . 'CongresoSeminarioSimponsio.pdf')) {
+    if (move_uploaded_file($guardado, 'Congreso Seminario Simponsio/' . $num_control . '-' . $apellido . '-' . $id_act . '.pdf')) {
 
-      $inserta = "INSERT INTO evidencia(numero_control, id_evento, subido, ruta_doc, responsable) VALUES ('$num_control', '$id_act', 1, 'Congreso Seminario Simponsio/$num_control-$nombre_alumno-$id_act-CongresoSeminarioSimponsio.pdf', '$responsable')";
+      $inserta = "INSERT INTO evidencia(numero_control, id_evento, subido, ruta_doc) VALUES ('$num_control', '$id_act', 1, 'Congreso Seminario Simponsio/$num_control-$apellido-$id_act.pdf')";
       $resultado = mysqli_query($conexion, $inserta);
       if (!$resultado) {
         echo 'Error al insertar archivo';

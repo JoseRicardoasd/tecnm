3212cf4073ae28c2ac4b56de5b6d75e9d0ada08f
AntonioQk <86135476+AntonioQk@users.noreply.github.com>
2022-10-05 10:45:00 -0500

    Update conferencia-platica.php
    cambios del responsable

--- usuarios/cargas/conferencia-platica.php
+++ usuarios/cargas/conferencia-platica.php
@@ -5,23 +5,23 @@ include('../../app/config/config.php');
 $nombre = $_FILES['archivo']['name'];
 $guardado = $_FILES['archivo']['tmp_name'];
 
-$id_act = $_POST['id_actividad'];
+$id_act = $_POST['evento'];
 
 $num_control = $_POST['numero_control'];
 
-$responsable = $_POST['responsable'];
+//$responsable = $_POST['responsable'];
 
-$nombre_alumno = $_POST['nombre_alumno'];
+$apellido = $_POST['ap_paterno'];
 
 if (!file_exists('Conferencia Platica')) {
   mkdir('Conferencia Platica', 0777, true);
   if (file_exists('Conferencia Platica')) {
-    if (file_exists('Conferencia Platica/' . $num_control . '-' . $nombre_alumno . '-' . $id_act . '-' . 'ConferenciaPlatica.pdf')) {
+    if (file_exists('Conferencia Platica/' . $num_control . '-' . $apellido . '-' . $id_act . '.pdf')) {
       echo '<script language="javascript">alert("El archivo ya existe");window.location.href="../agregar-credito-user.php"</script>';
     } else {
-      if (move_uploaded_file($guardado, 'Conferencia Platica/' . $num_control . '-' . $nombre_alumno . '_' . $id_act . '-' . 'ConferenciaPlatica.pdf')) {
+      if (move_uploaded_file($guardado, 'Conferencia Platica/' . $num_control . '-' . $apellido . '-' . $id_act . '.pdf')) {
 
-        $inserta = "INSERT INTO evidencia(numero_control, id_evento, subido, ruta_doc, responsable) VALUES ('$num_control', '$id_act', 1, 'Conferencia Platica/$num_control-$nombre_alumno-$id_act-ConferenciaPlatica.pdf', '$responsable')";
+        $inserta = "INSERT INTO evidencia(numero_control, id_evento, subido, ruta_doc) VALUES ('$num_control', '$id_act', 1, 'Conferencia Platica/$num_control-$apellido-$id_act.pdf')";
         $resultado = mysqli_query($conexion, $inserta);
         if (!$resultado) {
           echo 'Error al insertar archivo';
@@ -35,12 +35,12 @@ if (!file_exists('Conferencia Platica')) {
     }
   }
 } else {
-  if (file_exists('Conferencia Platica/' . $num_control . '-' . $nombre_alumno . '-' . $id_act . '-' . 'ConferenciaPlatica.pdf')) {
+  if (file_exists('Conferencia Platica/' . $num_control . '-' . $apellido . '-' . $id_act . '.pdf')) {
     echo '<script language="javascript">alert("El archivo ya existe");window.location.href="../agregar-credito-user.php"</script>';
   } else {
-    if (move_uploaded_file($guardado, 'Conferencia Platica/' . $num_control . '-' . $nombre_alumno . '-' . $id_act . '-' . 'ConferenciaPlatica.pdf')) {
+    if (move_uploaded_file($guardado, 'Conferencia Platica/' . $num_control . '-' . $apellido . '-' . $id_act . '.pdf')) {
 
-      $inserta = "INSERT INTO evidencia(numero_control, id_evento, subido, ruta_doc, responsable) VALUES ('$num_control', '$id_act', 1, 'Conferencia Platica/$num_control-$nombre_alumno-$id_act-ConferenciaPlatica.pdf', '$responsable')";
+      $inserta = "INSERT INTO evidencia(numero_control, id_evento, subido, ruta_doc) VALUES ('$num_control', '$id_act', 1, 'Conferencia Platica/$num_control-$apellido-$id_act.pdf')";
       $resultado = mysqli_query($conexion, $inserta);
       if (!$resultado) {
         echo 'Error al insertar archivo';

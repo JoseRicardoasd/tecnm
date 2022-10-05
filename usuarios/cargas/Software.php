78ad453051f66469495d947486bbf2362d59e227
AntonioQk <86135476+AntonioQk@users.noreply.github.com>
2022-10-05 10:41:43 -0500

    Update Software.php
    cambios del responsable

--- usuarios/cargas/Software.php
+++ usuarios/cargas/Software.php
@@ -7,14 +7,14 @@ $guardado = $_FILES['archivo']['tmp_name'];
 
 //se captura el id del evento
 
-$id_act = $_POST['id_actividad'];
+$id_act = $_POST['evento'];
 
 //captura del numero de control del usuario
 $num_control = $_POST['numero_control'];
 
-$responsable = $_POST['responsable'];
+//$responsable = $_POST['responsable'];
 
-$nombre_alumno = $_POST['nombre_alumno'];
+$apellido = $_POST['ap_paterno'];
 
 //se verifica si existe la carpeta don de se guardará el archivo
 if (!file_exists('Diseño de Software')) {
@@ -22,13 +22,13 @@ if (!file_exists('Diseño de Software')) {
   mkdir('Diseño de Software', 0777, true);
   if (file_exists('Diseño de Software')) {
     //Se verifica que el archivo que se sube, no exista en la carpeta, si ya existe no se podrá subir otra vez
-    if (file_exists('Diseño de Software/' . $num_control . '-' . $nombre_alumno . '-' . $id_act . '-' . 'DiseñoSoftware.pdf')) {
+    if (file_exists('Diseño de Software/' . $num_control . '-' . $apellido . '-' . $id_act . '.pdf')) {
       echo '<script language="javascript">alert("El archivo ya existe");window.location.href="../agregar-credito-user.php"</script>';
     } else {
       //Una vez que se confirmó que no se ha subido antes el archivo, se guarda el que se está subiendo a la carpeta que le corresponde
-      if (move_uploaded_file($guardado, 'Diseño de Software/' . $num_control . '-' . $nombre_alumno . '-' . $id_act . '-' . 'DiseñoSoftware.pdf')) {
+      if (move_uploaded_file($guardado, 'Diseño de Software/' . $num_control . '-' . $apellido . '-' . $id_act . '.pdf')) {
 
-        $inserta = "INSERT INTO evidencia(numero_control, id_evento, subido, ruta_doc, responsable) VALUES ('$num_control', '$id_act', 1, 'Diseño de Software/$num_control-$nombre_alumno-$id_act-DiseñoSoftware.pdf', '$responsable')";
+        $inserta = "INSERT INTO evidencia(numero_control, id_evento, subido, ruta_doc) VALUES ('$num_control', '$id_act', 1, 'Diseño de Software/$num_control-$apellido-$id_act.pdf')";
         $resultado = mysqli_query($conexion, $inserta);
         if (!$resultado) {
           echo 'Error al insertar archivo';
@@ -43,13 +43,13 @@ if (!file_exists('Diseño de Software')) {
   }
 } else {
   //Se verifica que el archivo que se sube, no exista en la carpeta, si ya existe no se podrá subir otra vez
-  if (file_exists('Diseño de Software/' . $num_control . '-' . $nombre_alumno . '-' . $id_act . '-' . 'DiseñoSoftware.pdf')) {
+  if (file_exists('Diseño de Software/' . $num_control . '-' . $apellido . '-' . $id_act . '.pdf')) {
     echo '<script language="javascript">alert("El archivo ya existe");window.location.href="../agregar-credito-user.php"</script>';
   } else {
     //Una vez que se confirmó que no se ha subido antes el archivo, se mueve el que se está subiendo a la carpeta que le corresponde
-    if (move_uploaded_file($guardado, 'Diseño de Software/' . $num_control . '-' . $nombre_alumno . '-' . $id_act . '-' . 'DiseñoSoftware.pdf')) {
+    if (move_uploaded_file($guardado, 'Diseño de Software/' . $num_control . '-' . $apellido . '-' . $id_act . '.pdf')) {
 
-      $inserta = "INSERT INTO evidencia(numero_control, id_evento, subido, ruta_doc, responsable) VALUES ('$num_control', '$id_act', 1, 'Diseño de Software/$num_control-$nombre_alumno-$id_act-DiseñoSoftware.pdf', '$responsable')";
+      $inserta = "INSERT INTO evidencia(numero_control, id_evento, subido, ruta_doc) VALUES ('$num_control', '$id_act', 1, 'Diseño de Software/$num_control-$apellido-$id_act.pdf')";
       $resultado = mysqli_query($conexion, $inserta);
       if (!$resultado) {
         echo 'Error al insertar archivo';

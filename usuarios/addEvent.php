8e3d0c240ae41bd9e3e2860715d38545d1e34fa6
AntonioQk <86135476+AntonioQk@users.noreply.github.com>
2022-10-05 11:50:20 -0500

    Update addEvent.php
    cambios del responsable

--- usuarios/addEvent.php
+++ usuarios/addEvent.php
@@ -2,16 +2,41 @@
 
 include('../app/config/config.php');
 
-if (isset($_POST['title']) && isset($_POST['descripcion']) && isset($_POST['start']) && isset($_POST['end']) && isset($_POST['color'])) {
+if (isset($_POST['title']) && isset($_POST['descripcion']) && isset($_POST['start']) && isset($_POST['end']) && isset($_POST['color']) && isset($_POST['respons'])) {
 
   $title = $_POST['title'];
   $descripcion = $_POST['descripcion'];
   $start = $_POST['start'];
   $end = $_POST['end'];
   $color = $_POST['color'];
-  $credito = $_POST['credito'];
+  $respons = $_POST['respons'];
+  
+  $tipo = "";
 
-  $sql = "INSERT INTO events(title, descripcion, start, end, color, credito_act) values ('$title','$descripcion', '$start', '$end', '$color', '$credito')";
+  if ($color == "#0071c5") {
+    $tipo = "Modalidad Academica";
+  } else if ($color == "#40E0D0") {
+    $tipo = "Conferencia y/o platica";
+  } else if ($color == "#008000") {
+    $tipo = "Congreso, Seminario, Etc.";
+  } else if ($color == "#FFD700") {
+    $tipo = "Curso y/o taller";
+  } else if ($color == "#FF8C00") {
+    $tipo = "Concurso de ciencias basicas";
+  } else if ($color == "#FF0000") {
+    $tipo = "Creatividad e innovacion";
+  } else if ($color == "#000") {
+    $tipo = "Concurso de emprendedurismo";
+  } else if ($color == "#c0392b") {
+    $tipo = "Diseño de prototipos";
+  } else if ($color == "#8e44ad") {
+    $tipo = "Diseño de software";
+  } else if ($color == "#2c3e50") {
+    $tipo = "Diseño de proyectos";
+  }
+  
+
+  $sql = "INSERT INTO events(title, descripcion, start, end, color, respons, tipo) values ('$title','$descripcion', '$start', '$end', '$color', '$respons', '$tipo')";
 
   echo $sql;
 

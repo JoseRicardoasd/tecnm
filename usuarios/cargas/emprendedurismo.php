293a200acff256555041a4c33e564b189ff65dc9
AntonioQk <86135476+AntonioQk@users.noreply.github.com>
2022-10-05 11:39:01 -0500

    Update emprendedurismo.php
    cambios del responsable

--- usuarios/cargas/emprendedurismo.php
+++ usuarios/cargas/emprendedurismo.php
@@ -16,12 +16,12 @@ $apellido = $_POST['ap_paterno'];
 if (!file_exists('Concurso de emprendedurismo')) {
   mkdir('Concurso de emprendedurismo', 0777, true);
   if (file_exists('Concurso de emprendedurismo')) {
-    if (file_exists('Concurso de emprendedurismo/' . $num_control . '-' . $nombre_alumno . '-' . $id_act . '-' . 'ConcursoEmprendedurismo.pdf')) {
+    if (file_exists('Concurso de emprendedurismo/' . $num_control . '-' . $apellido . '-' . $id_act . '.pdf')) {
       echo '<script language="javascript">alert("El archivo ya existe");window.location.href="../agregar-credito-user.php"</script>';
     } else {
-      if (move_uploaded_file($guardado, 'Concurso de emprendedurismo/' . $num_control . '-' . $nombre_alumno . '-' . $id_act . '-' . 'ConcursoEmprendedurismo.pdf')) {
+      if (move_uploaded_file($guardado, 'Concurso de emprendedurismo/' . $num_control . '-' . $apellido . '-' . $id_act . '.pdf')) {
         //insertar datos en la tabla
-        $inserta = "INSERT INTO evidencia(numero_control, id_evento, subido, ruta_doc, responsable) VALUES ('$num_control', '$id_act', 1, 'Concurso de emprendedurismo/$num_control-$nombre_alumno-$id_act-ConcursoEmprendedurismo.pdf', '$responsable')";
+        $inserta = "INSERT INTO evidencia(numero_control, id_evento, subido, ruta_doc) VALUES ('$num_control', '$id_act', 1, 'Concurso de emprendedurismo/$num_control-$apellido-$id_act.pdf')";
         $resultado = mysqli_query($conexion, $inserta);
         if (!$resultado) {
           echo 'Error al insertar archivo';
@@ -35,12 +35,12 @@ if (!file_exists('Concurso de emprendedurismo')) {
     }
   }
 } else {
-  if (file_exists('Concurso de emprendedurismo/' . $num_control . '-' . $nombre_alumno . '-' . $id_act . '-' . 'ConcursoEmprendedurismo.pdf')) {
+  if (file_exists('Concurso de emprendedurismo/' . $num_control . '-' . $apellido . '-' . $id_act . '.pdf')) {
     echo '<script language="javascript">alert("El archivo ya existe");window.location.href="../agregar-credito-user.php"</script>';
   } else {
-    if (move_uploaded_file($guardado, 'Concurso de emprendedurismo/' . $num_control . '-' . $nombre_alumno . '-' . $id_act . '-' . 'ConcursoEmprendedurismo.pdf')) {
+    if (move_uploaded_file($guardado, 'Concurso de emprendedurismo/' . $num_control . '-' . $apellido . '-' . $id_act . '.pdf')) {
       //insertar datos en la tabla
-      $inserta = "INSERT INTO evidencia(numero_control, id_evento, subido, ruta_doc, responsable) VALUES ('$num_control', '$id_act', 1, 'Concurso de emprendedurismo/$num_control-$nombre_alumno-$id_act-ConcursoEmprendedurismo.pdf', '$responsable')";
+      $inserta = "INSERT INTO evidencia(numero_control, id_evento, subido, ruta_doc) VALUES ('$num_control', '$id_act', 1, 'Concurso de emprendedurismo/$num_control-$apellido-$id_act.pdf')";
       $resultado = mysqli_query($conexion, $inserta);
       if (!$resultado) {
         echo 'Error al insertar archivo';

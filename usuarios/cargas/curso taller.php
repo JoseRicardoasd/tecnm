18e386bcd2dd586d3a25333bb9993991e65653de
AntonioQk <86135476+AntonioQk@users.noreply.github.com>
2022-10-05 10:53:01 -0500

    Update curso taller.php
    cambios del responsable

--- usuarios/cargas/curso taller.php
+++ usuarios/cargas/curso taller.php
@@ -5,24 +5,24 @@ include('../../app/config/config.php');
 $nombre = $_FILES['archivo']['name'];
 $guardado = $_FILES['archivo']['tmp_name'];
 
-$id_act = $_POST['id_actividad'];
+$id_act = $_POST['evento'];
 
 $num_control = $_POST['numero_control'];
 
 
-$responsable = $_POST['responsable'];
+//$responsable = $_POST['responsable'];
 
-$nombre_alumno = $_POST['nombre_alumno'];
+$apellido = $_POST['ap_paterno'];
 
 if (!file_exists('Curso o curso taller')) {
   mkdir('Curso o curso taller', 0777, true);
   if (file_exists('Curso o curso taller')) {
-    if (file_exists('Curso o curso taller/' . $num_control . '-' . $nombre_alumno . '-' . $id_act . '-' . 'cursoTaller.pdf')) {
+    if (file_exists('Curso o curso taller/' . $num_control . '-' . $apellido . '-' . $id_act . '.pdf')) {
       echo '<script language="javascript">alert("El archivo ya existe");window.location.href="../agregar-credito-user.php"</script>';
     } else {
-      if (move_uploaded_file($guardado, 'Curso o curso taller/' . $num_control . '-' . $nombre_alumno . '-' . $id_act . '-' . 'cursoTaller.pdf')) {
+      if (move_uploaded_file($guardado, 'Curso o curso taller/' . $num_control . '-' . $apellido . '-' . $id_act . '.pdf')) {
 
-        $inserta = "INSERT INTO evidencia(numero_control, id_evento, subido, ruta_doc, responsable) VALUES ('$num_control', '$id_act', 1, 'Curso o curso taller/$num_control-$nombre_alumno-$id_act-cursoTaller.pdf', '$responsable')";
+        $inserta = "INSERT INTO evidencia(numero_control, id_evento, subido, ruta_doc) VALUES ('$num_control', '$id_act', 1, 'Curso o curso taller/$num_control-$apellido-$id_act.pdf')";
         $resultado = mysqli_query($conexion, $inserta);
         if (!$resultado) {
           echo 'Error al insertar archivo';
@@ -37,12 +37,12 @@ if (!file_exists('Curso o curso taller')) {
   }
 } else {
 
-  if (file_exists('Curso o curso taller/' . $num_control . '-' . $nombre_alumno . '-' . $id_act . '-' . 'cursoTaller.pdf')) {
+  if (file_exists('Curso o curso taller/' . $num_control . '-' . $apellido . '-' . $id_act . '.pdf')) {
     echo '<script language="javascript">alert("El archivo ya existe");window.location.href="../agregar-credito-user.php"</script>';
   } else {
-    if (move_uploaded_file($guardado, 'Curso o curso taller/' . $num_control . '-' . $nombre_alumno . '-' . $id_act . '-' . 'cursoTaller.pdf')) {
+    if (move_uploaded_file($guardado, 'Curso o curso taller/' . $num_control . '-' . $apellido . '-' . $id_act . '.pdf')) {
 
-      $inserta = "INSERT INTO evidencia(numero_control, id_evento, subido, ruta_doc, responsable) VALUES ('$num_control', '$id_act', 1, 'Curso o curso taller/$num_control-$nombre_alumno-$id_act-cursoTaller.pdf', '$responsable')";
+      $inserta = "INSERT INTO evidencia(numero_control, id_evento, subido, ruta_doc) VALUES ('$num_control', '$id_act', 1, 'Curso o curso taller/$num_control-$apellido-$id_act.pdf')";
       $resultado = mysqli_query($conexion, $inserta);
       if (!$resultado) {
         echo 'Error al insertar archivo';

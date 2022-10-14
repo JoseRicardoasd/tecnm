32e0596f7b656f2066a5d59d6e419c48c35fbbe9
AntonioQk <86135476+AntonioQk@users.noreply.github.com>
2022-10-14 11:34:43 -0500

    Update menu.php

--- layout/menu.php
+++ layout/menu.php
@@ -100,55 +100,92 @@
 <!-- Left side column. contains the logo and sidebar -->
 <aside class="main-sidebar">
   <!-- sidebar: style can be found in sidebar.less -->
-  <section class="sidebar">
+  <section class="sidebar sidebar-menu">
     <!-- Sidebar user panel -->
+    <!-- <div class="user-panel">
+      <div class="pull-left image">
+        <?php
+        $caracter_a_buscar = ".";
+        $buscar = strpos($id_foto_perfil, $caracter_a_buscar);
+        if ($buscar == true) {
+          // echo "existe foto de perfil";
+        ?>
+          <img src="<?php echo $URL; ?>../usuarios/update_usuarios/<?php echo $sesion_usuario['foto_perfil']; ?>" class="user-image" alt="User Image">
+          <?php
+        } else {
+          if ($id_sexo  == "HOMBRE") {
+          ?>
+
+            <img src="<?php echo $URL; ?>../public/images/avatar_hombre.png" class="user-image" alt="User Image">
+
+          <?php
+          } else {
+          ?>
+
+            <img src="<?php echo $URL; ?>../public/images/avatar_mujer.png" class="user-image" alt="User Image">
+
+        <?php
+          }
+        }
+        ?>
+      </div>
+      <div class="pull-left info">
+        <p><?php echo $id_nombres . " " . $id_ap_paterno . " " . $id_ap_materno; ?></p>
+        <a href="#"><i class="fa fa-circle text-success"></i> Online</a>
+      </div>
+    </div><br> -->
 
     <!-- /.search form -->
     <!-- sidebar menu: : style can be found in sidebar.less -->
-    <ul class="sidebar-menu">
+    <li class="treeview">
+      <a href="#">
+        <i class="fa fa-users"></i> <span>AREA ACADEMICA</span>
+        <span class="pull-right-container">
+          <i class="fa fa-angle-left pull-right"></i>
+        </span>
+      </a>
+      <ul class=" sidebar-menu treeview-menu">
+
+        <li class="treeview">
+          <a href="#">
+            <i class="fa fa-users"></i> <span>Agregar Usuarios</span>
+            <span class="pull-right-container">
+              <i class="fa fa-angle-left pull-right"></i>
+            </span>
+          </a>
+          <ul class="treeview-menu">
+
+            <li><a href="lista-usuarios.php"><i class="fa fa-users"></i> Lista de Usuarios</a></li>
+
+            <li><a href="create.php"><i class="fa fa-user"></i> Agregar Alumno</a></li>
 
 
-      <li class="header">Bienvenido</li>
+            <li><a href="create_usuario.php"><i class="fa fa-user"></i> Agregar Usuario</a></li>
+
+          </ul>
+        </li>
 
 
 
-      <li class="treeview">
-        <a href="#">
-          <i class="fa fa-users"></i> <span>Agregar Usuarios</span>
-          <span class="pull-right-container">
-            <i class="fa fa-angle-left pull-right"></i>
-          </span>
-        </a>
-        <ul class="treeview-menu">
+        <li class="header">Calendario de Actividades</li>
+        <li><a href="calendario.php"><i class="glyphicon glyphicon-calendar"></i> <span>Calendario</span></a></li>
+        <li class="header">talleres</li>
+        <li><a href="talleres.php"><i class="glyphicon glyphicon-calendar"></i> <span>Talleres</span></a></li>
 
-          <li><a href="lista-usuarios.php"><i class="fa fa-users"></i> Lista de Usuarios</a></li>
-
-          <li><a href="create.php"><i class="fa fa-user"></i> Agregar Alumno</a></li>
-
-
-          <li><a href="create_usuario.php"><i class="fa fa-user"></i> Agregar Usuario</a></li>
-
-        </ul>
-      </li>
-
+        <li class="header">Formatos</li>
+        <li><a href="guia.php"><i class="glyphicon glyphicon-paperclip"></i> <span>Guia de Actividades</span></a></li>
+        <li><a href="agregar-credito.php"><i class="glyphicon glyphicon-folder-open"></i> <span>Agregar Credito</span></a></li>
+        <li class="header">Evaluciones</li>
+        <li><a href="evaluacion.php"><i class="fa fa-book"></i> <span>Evalucion de Desempeño</span></a></li>
+        <li><a href="constancia.php"><i class="fa fa-book"></i> <span>Constancias</span></a></li>
+        <li><a href="generarconstancia.php"><i class="fa fa-book"></i> <span>GenerarConstancias</span></a></li>
+        <li class="header">Tutorias</li>
+        <li><a href="tutoria.php"><i class="fa fa-user"></i> <span>registro de tutoriados</span></a></li>
+        <li><a href="lista_tutoria.php"><i class="fa fa-users"></i> <span>Lista de Tutorias</span></a></li>
+      </ul>
+    </li>
 
 
-      <li class="header">Calendario de Actividades</li>
-      <li><a href="calendario.php"><i class="glyphicon glyphicon-calendar"></i> <span>Calendario</span></a></li>
-      <li class="header">talleres</li>
-      <li><a href="talleres.php"><i class="glyphicon glyphicon-calendar"></i> <span>Talleres</span></a></li>
-
-      <li class="header">Formatos</li>
-      <li><a href="guia.php"><i class="glyphicon glyphicon-paperclip"></i> <span>Guia de Actividades</span></a></li>
-      <li><a href="agregar-credito.php"><i class="glyphicon glyphicon-folder-open"></i> <span>Agregar Credito</span></a></li>
-      <li class="header">Evaluciones</li>
-      <li><a href="evaluacion.php"><i class="fa fa-book"></i> <span>Evalucion de Desempeño</span></a></li>
-      <li><a href="constancia.php"><i class="fa fa-book"></i> <span>Constancias</span></a></li>
-      <li><a href="generarconstancia.php"><i class="fa fa-book"></i> <span>GenerarConstancias</span></a></li>
-      <li class="header">Tutorias</li>
-      <li><a href="tutoria.php"><i class="fa fa-user"></i> <span>registro de tutoriados</span></a></li>
-      <li><a href="lista_tutoria.php"><i class="fa fa-users"></i> <span>Lista de Tutorias</span></a></li>
-    </ul>
 
   </section>
   <!-- /.sidebar -->

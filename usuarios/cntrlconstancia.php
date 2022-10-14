8a40f72df3d7903fd6a75e8106dbd3846d971a52
AntonioQk <86135476+AntonioQk@users.noreply.github.com>
2022-10-13 22:22:31 -0500

    Update cntrlconstancia.php

--- usuarios/cntrlconstancia.php
+++ usuarios/cntrlconstancia.php
@@ -1,23 +1,27 @@
 <?php
 
 include('../app/config/config.php');
-//traemos las variables
-$ciudadano = $_POST['ciudadano'];
-$suscribe = $_POST['suscribe'];
-$alumno = $_POST['alumno'];
-$matricula = $_POST['matricula'];
-$carrera = $_POST['carrera'];
-$desempe = $_POST['desempe'];
-$valor = $_POST['valor'];
-$ciclo = $_POST['ciclo'];
-$valorcurri = $_POST['valorcurri'];
-$ciudad = $_POST['ciudad'];
-$dias = $_POST['dias'];
-$mes = $_POST['mes'];
-$anio = $_POST['anio'];
 
-//sentencia sql
-$sql = "INSERT INTO constancias (ciudadano,
+if (isset($_POST['suscribe']) && isset($_POST['alumno']) && isset($_POST['matricula']) && isset($_POST['carrera']) && isset($_POST['desemp']) && isset($_POST['valor']) && isset($_POST['ciclo']) && isset($_POST['valorcurri']) && isset($_POST['dias']) && isset($_POST['mes']) && isset($_POST['anio'])) {
+
+
+  //traemos las variables
+  //$ciudadano = $_POST['ciudadano'];
+  $suscribe = $_POST['suscribe'];
+  $alumno = $_POST['alumno'];
+  $matricula = $_POST['matricula'];
+  $carrera = $_POST['carrera'];
+  $desempe = $_POST['desempe'];
+  $valor = $_POST['valor'];
+  $ciclo = $_POST['ciclo'];
+  $valorcurri = $_POST['valorcurri'];
+  //$ciudad = $_POST['ciudad'];
+  $dias = $_POST['dias'];
+  $mes = $_POST['mes'];
+  $anio = $_POST['anio'];
+
+  //sentencia sql
+  $sql = "INSERT INTO constancias (/*ciudadano,*/
                                 suscribe,
                                 alumno,
                                 matricula,
@@ -26,12 +30,12 @@ $sql = "INSERT INTO constancias (ciudadano,
                                 valor,
                                 ciclo,
                                 valorcurri,
-                                ciudad,
+                                /*ciudad,*/
                                 dias,
                                 mes,
                                 anio) 
                                 VALUES 
-                                ('$ciudadano',
+                                (
                                        '$suscribe',
                                        '$alumno',
                                        '$matricula',
@@ -40,19 +44,20 @@ $sql = "INSERT INTO constancias (ciudadano,
                                        '$valor',
                                        '$ciclo',
                                        '$valorcurri',
-                                       '$ciudad',
+                                       
                                        '$dias',
                                        '$mes',
                                        '$anio')";
 
 
-//ejecutamos sql
+  //ejecutamos sql
 
-$ejecutar = mysqli_query($conexion, $sql);
-//verificamos la ejecucion
-if (!$ejecutar) {
-  echo 'Error al registrarse';
-} else {
-  header('Location: constancia.php');
+  $ejecutar = mysqli_query($conexion, $sql);
+  //verificamos la ejecucion
+  if (!$ejecutar) {
+    echo '<script language="javascript">alert("No se pudo guardar la constancia");window.location.href="constancia.php"</script>';
+  } else {
+    echo '<script language="javascript">alert("Constancia guardada satisfactoriamente");window.location.href="constancia.php"</script>';
+  }
+  mysqli_close($conexion);
 }
-mysqli_close($conexion);

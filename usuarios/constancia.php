54dfcd7e2d134ffdc8e90ce9858ff7cc16e6418e
AntonioQk <86135476+AntonioQk@users.noreply.github.com>
2022-10-13 22:19:18 -0500

    Update constancia.php

--- usuarios/constancia.php
+++ usuarios/constancia.php
@@ -1,7 +1,8 @@
 <?php
 include('../app/config/config.php');
 session_start();
-if (isset($_SESSION['u_usuario'])) {
+if (isset($_SESSION['u_usuario']) && $_SESSION['u_privilegio']  == 0) {
+  //echo "existe sesión";) {
   $correo_sesion = $_SESSION['u_usuario'];
   $query_sesion = $pdo->prepare("SELECT * FROM tb_usuarios WHERE correo = '$correo_sesion' AND estado = '1' ");
   $query_sesion->execute();
@@ -58,7 +59,7 @@ if (isset($_SESSION['u_usuario'])) {
           <div class="panel panel-primary">
             <div class="panel-heading">Capatura de Datos Para la Constancia</div>
             <div class="panel-body">
-              <table class="table table-bordered table-hover table-condensed">
+              <!-- <table class="table table-bordered table-hover table-condensed">
 
                 <body>
                   <div class="container">
@@ -66,47 +67,56 @@ if (isset($_SESSION['u_usuario'])) {
                     <div class="row">
                       <form action="cntrlconstancia.php" method="POST">
                     </div>
-                    <h2> CONSTANCIA DE CUMPLIMIENTO DE ACTIVIDAD COMPLEMENTARIA.</h2>
-                    <br>
 
 
-                    DRA. ELOINA GUADALUPE GONZÁLEZ LARA
-                    <br>
+              </table> -->
 
-                    Jefe(a) del Departamento de Servicios Escolares o su equivalente en los Institutos Tecnológicos Descentralizados
-
-                    <br>
-                    PRESENTE.
-                    <br>
-                    <br>
-                    <br>
-                    <p style="text-align: justify;">
-                      El que se suscribe <input type="text" name="suscribe" maxlength="100" style="width:500px; height:20x;">, por
-                      este medio se permite hacer de su conocimiento que
-                      <br> el estudiante <input type="text" name="alumno" maxlength="100" style="width:500px; height:20x;">
-                      con número de control <input type="text" name="matricula" maxlength="100" style="width:150px; height:20x;"><br>
-                      de la carrera de <input type="text" name="carrera" maxlength="100" style="width:500px; height:20x;">
-                      ha cumplido su actividad complementaria con el nivel de desempeño<br> <input type="text" name="desempe" maxlength="100" style="width:100px; height:20x;">
-                      y un valor numérico de <input type="text" name="valor" maxlength="100" style="width:100px; height:20x;">,
-                      durante el periodo escolar <input type="text" name="ciclo" maxlength="100" style="width:150px; height:20x;">
-                      con un valor curricular de <input type="text" name="valorcurri" maxlength="100" style="width:50px; height:20x;">
-                      créditos.
-                    </p>
-                    <br>
-                    <br>
-                    <br>
-                    Se extiende la presente en el poblado de Chiná a los
-                    <?php
+              <form action="cntrlconstancia.php" method="POST">
+                <h2> CONSTANCIA DE CUMPLIMIENTO DE ACTIVIDAD COMPLEMENTARIA.</h2>
+                <br>
 
 
-                    setlocale(LC_ALL, "es_ES");
-                    echo   date("d") . " dias de  " . date("F") . " de " . date("Y"); ?>
+                DRA. ELOINA GUADALUPE GONZÁLEZ LARA
+                <br>
 
-                    <center>
-                      <td colspan="6"><button name="enviar" value="enviar" class="btn btn-success"> Guardar</button></td>
-                    </center>
+                Jefe(a) del Departamento de Servicios Escolares o su equivalente en los Institutos Tecnológicos Descentralizados
 
-              </table>
+                <br>
+                PRESENTE.
+                <br>
+                <br>
+                <br>
+                <p style="text-align: justify;">
+                  El que se suscribe <input type="text" name="suscribe" maxlength="100" style="width:500px; height:20x; margin:3px;">, por
+                  este medio se permite hacer de su conocimiento que
+                  <br> el estudiante <input type="text" name="alumno" maxlength="100" style="width:500px; height:20x; margin:3px">
+                  con número de control <input type="text" name="matricula" maxlength="100" style="width:150px; height:20x; margin:3px"><br>
+                  de la carrera de <input type="text" name="carrera" maxlength="100" style="width:500px; height:20x; margin:3px">
+                  ha cumplido su actividad complementaria con el nivel de desempeño<br> <input type="text" name="desempe" maxlength="100" style="width:100px; height:20x; margin:3px">
+                  y un valor numérico de <input type="text" name="valor" maxlength="100" style="width:100px; height:20x; margin:3px">,
+                  durante el periodo escolar <input type="text" name="ciclo" maxlength="100" style="width:150px; height:20x; margin:3px">
+                  con un valor curricular de <input type="text" name="valorcurri" maxlength="100" style="width:50px; height:20x; margin:3px">
+                  créditos.
+                </p>
+                <br>
+                <br>
+                <br>
+                Se extiende la presente en el poblado de Chiná a los
+                <?php
+                setlocale(LC_ALL, "es_ES");
+                $dias = date("d");
+                $mes = date("F");
+                $anio = date("Y");
+
+                echo   $dias . " dias de  " . $mes . " de " . $anio; ?>
+
+                <center>
+                  <td colspan="6"><button name="enviar" value="enviar" class="btn btn-success"> Guardar</button></td>
+                </center>
+                <input type="text" name="dias" value="<?php echo $dias ?>" style="display: none;">
+                <input type="text" name="mes" value="<?php echo $mes ?>" style="display: none;">
+                <input type="text" name="anio" value="<?php echo $anio ?>" style="display: none;">
+              </form>
               <div id="content" class="col-lg-12">
 
               </div>

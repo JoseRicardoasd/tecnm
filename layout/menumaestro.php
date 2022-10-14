36c3ee57f19db9c029ca0919e677be7ef0d13f8c
AntonioQk <86135476+AntonioQk@users.noreply.github.com>
2022-10-14 11:35:20 -0500

    Update menumaestro.php

--- layout/menumaestro.php
+++ layout/menumaestro.php
@@ -103,40 +103,79 @@
   </header>
   <!-- Left side column. contains the logo and sidebar -->
   <aside class="main-sidebar">
-    <!-- sidebar: style can be found in sidebar.less -->
-    <section class="sidebar">
-      <!-- Sidebar user panel -->
-      
-      <!-- /.search form -->
-      <!-- sidebar menu: : style can be found in sidebar.less -->
-      <ul class="sidebar-menu">
-        
-        
-        <li class="header">Bienvenido</li>
-        
-       
-       
-           <li class="treeview">
-          
-  
+  <!-- sidebar: style can be found in sidebar.less -->
+  <section class="sidebar sidebar-menu">
+    <!-- Sidebar user panel -->
+    <!-- <div class="user-panel">
+      <div class="pull-left image">
+        <?php
+        $caracter_a_buscar = ".";
+        $buscar = strpos($id_foto_perfil, $caracter_a_buscar);
+        if ($buscar == true) {
+          // echo "existe foto de perfil";
+        ?>
+          <img src="<?php echo $URL; ?>/usuarios/update_usuarios/<?php echo $sesion_usuario['foto_perfil']; ?>" class="user-image" alt="User Image">
+          <?php
+        } else {
+          if ($id_sexo  == "HOMBRE") {
+          ?>
+
+            <img src="<?php echo $URL; ?>/public/images/avatar_hombre.png" class="user-image" alt="User Image">
+
+          <?php
+          } else {
+          ?>
+
+            <img src="<?php echo $URL; ?>/public/images/avatar_mujer.png" class="user-image" alt="User Image">
+
+        <?php
+          }
+        }
+        ?>
+      </div>
+      <div class="pull-left info">
+        <p><?php echo $id_nombres . " " . $id_ap_paterno . " " . $id_ap_materno; ?></p>
+        <a href="#"><i class="fa fa-circle text-success"></i> Online</a>
+      </div>
+    </div><br> -->
+
+    <!-- /.search form -->
+    <!-- sidebar menu: : style can be found in sidebar.less -->
+    <li class="treeview">
+      <a href="#">
+        <i class="fa fa-users"></i> <span>AREA ACADEMICA</span>
+        <span class="pull-right-container">
+          <i class="fa fa-angle-left pull-right"></i>
+        </span>
+      </a>
+      <ul class="sidebar-menu treeview-menu">
+
+
+        <!-- <li class="header">Bienvenido</li> -->
+
+
+
+        <li class="treeview">
+
+
 
 
         <li class="header">Calendario de Actividades</li>
         <li><a href="calendariovis.php"><i class="glyphicon glyphicon-calendar"></i> <span>Calendario</span></a></li>
         <li class="header">talleres</li>
-        
-        
+
+
         <li class="header">Formatos</li>
         <li><a href="guiamaestro.php"><i class="glyphicon glyphicon-paperclip"></i> <span>Guia de Actividades</span></a></li>
-        
+
         <li class="header">Evaluciones</li>
         <li><a href="evaluacionmaestro.php"><i class="fa fa-book"></i> <span>Evalucion de Desempeño</span></a></li>
-   
-      
-      </ul>
 
-    </section>
-    <!-- /.sidebar -->
-  </aside>
- 
+
+      </ul>
+    </li>
+
+  </section>
+  <!-- /.sidebar -->
+</aside>
  

9abffea2c25aaec17d37a22a5e6f161cdefe9d93
AntonioQk <86135476+AntonioQk@users.noreply.github.com>
2022-10-22 19:01:54 -0500

    Update constancia.php

--- usuarios/constancia.php
+++ usuarios/constancia.php
@@ -71,32 +71,60 @@ if (isset($_SESSION['u_usuario']) && $_SESSION['u_privilegio']  == 0) {
 
 
               </table> -->
+              <h2> CONSTANCIA DE CUMPLIMIENTO DE ACTIVIDAD COMPLEMENTARIA.</h2>
+              <br>
+              <!-- Opcion para ver cuantos creditos lleva el alumno (esta informacion es el resultado de la evalucion que realiza el maestro) -->
+              <div class="container_input">
+                <label for="matricula" class="label_input">Matricula:</label>
+                <input type="text" name="matricula2" id="buscar_matricula" maxlength="50" class="input_1">
+                <div value="buscar" class="btn-primary btn_buscar" onclick="datos_constancia();">Buscar</div>
+              </div>
+              <!-- tabla con los datos solicitados en el input anterior (matricula) -->
+              <table class="table table-bordered table-hover table-condensed">
+                <thead>
+                  <tr class="info">
+                    <th>Nombre Alumno</th>
+                    <th>Observaciones</th>
+                    <th>Desempeño</th>
+                    <th>Valor</th>
+                    <th>Evento o Actividad</th>
+                    <th>Credito</th>
+                  </tr>
+                </thead>
+                <tbody id="body_table">
+                </tbody>
+              </table>
 
+              <div class="docs" id="docs">
+                <p id="credito_valor">Creditos totales: </p>
+
+              </div>
+
+              <!-- formulario para guardar la constancia -->
               <form action="cntrlconstancia.php" method="POST">
-                <h2> CONSTANCIA DE CUMPLIMIENTO DE ACTIVIDAD COMPLEMENTARIA.</h2>
-                <br>
+                
+                <p style="margin-top:20px">
+                  DRA. ELOINA GUADALUPE GONZÁLEZ LARA
+                  <br>
 
+                  Jefe(a) del Departamento de Servicios Escolares o su equivalente en los Institutos Tecnológicos Descentralizados
 
-                DRA. ELOINA GUADALUPE GONZÁLEZ LARA
-                <br>
-
-                Jefe(a) del Departamento de Servicios Escolares o su equivalente en los Institutos Tecnológicos Descentralizados
-
-                <br>
-                PRESENTE.
+                  <br>
+                  PRESENTE.
+                </p>
                 <br>
                 <br>
                 <br>
                 <p class="parrafo_inputs">
-                  El que se suscribe <input type="text" name="suscribe" maxlength="100" class="input_border input_largo">, por
+                  El que se suscribe <input type="text" name="suscribe" maxlength="100" class="input_border input_largo" placeholder="Suscribe">, por
                   este medio se permite hacer de su conocimiento que
-                  <br> el estudiante <input type="text" name="alumno" maxlength="100" class="input_border input_largo">
-                  con número de control <input type="text" name="matricula" maxlength="100" class="input_border input_corto"><br>
-                  de la carrera de <input type="text" name="carrera" maxlength="100" class="input_border input_largo">
-                  ha cumplido su actividad complementaria con el nivel de desempeño<br> <input type="text" name="desempe" maxlength="100" class="input_border">
-                  y un valor numérico de <input type="text" name="valor" maxlength="100" class="input_border input_corto">,
-                  durante el periodo escolar <input type="text" name="ciclo" maxlength="100" class="input_border input_corto">
-                  con un valor curricular de <input type="text" name="valorcurri" maxlength="100" class="input_border input_corto">
+                  <br> el estudiante <input type="text" name="alumno" maxlength="100" class="input_border input_largo" placeholder="Nombre alumno">
+                  con número de control <input type="text" name="matricula" maxlength="100" class="input_border input_corto" placeholder="Matricula alumno"><br>
+                  de la carrera de <input type="text" name="carrera" maxlength="100" class="input_border input_largo" placeholder="Carrera del alumno">
+                  ha cumplido su actividad complementaria con el nivel de desempeño<br> <input type="text" name="desempe" maxlength="100" class="input_border" placeholder="Desempeño">
+                  y un valor numérico de <input type="text" name="valor" maxlength="100" class="input_border input_corto" placeholder="Valor">,
+                  durante el periodo escolar <input type="text" name="ciclo" maxlength="100" class="input_border input_corto" placeholder="Ciclo escolar">
+                  con un valor curricular de <input type="text" name="valorcurri" maxlength="100" class="input_border input_corto" placeholder="Valor Curricular">
                   créditos.
                 </p>
                 <br>
@@ -137,3 +165,96 @@ if (isset($_SESSION['u_usuario']) && $_SESSION['u_privilegio']  == 0) {
   echo "no existe sesión";
   header('Location:' . $URL . '/login');
 }
+
+<script>
+  function datos_constancia() {
+    buscar_matricula = $("#buscar_matricula").val();
+    //se manda la matricula al archivo php para buscar los datos enbase a esa matricula
+    var parametros = {
+      "buscar": "1",
+      "buscar_matricula": buscar_matricula
+    }; //fucnion que me manda esa matricula 
+    $.ajax({
+      data: parametros,
+      dataType: 'json',
+      url: 'constancia_datos.php',
+      type: 'post',
+      error: function() {
+        alert("Error");
+      },
+      //funcion que recibe los datos que el archivo "datos_alumno_evaluación.php" devuelve, esto los devuelve como un objeto llamado "valores"
+      success: function(datos) {
+        //console.log(datos);
+        const docs = document.getElementById("docs");
+        const body_tabla = document.getElementById("body_table");
+        datos.forEach(element => {
+          //se crean los elementos tr y td de la tabla
+          let tr = document.createElement('tr');
+          let td1 = document.createElement('td');
+
+          //se agregan los valores correspondientes en la posicion correspondiente de la tabla
+          td1.innerHTML = element[0];
+          let td2 = document.createElement('td');
+          td2.innerHTML = element[1];
+          let td3 = document.createElement('td');
+          td3.innerHTML = element[2];
+          let td4 = document.createElement('td');
+          td4.innerHTML = element[3];
+          //PDFS---------------
+          let div = document.createElement('div');
+          div.style = "height:30px; margin-right:30px";
+          let img = document.createElement('img');
+          img.src = "../images/pdf_logo.png";
+          img.style = "width:20px; heigth:20px; margin-right:7px";
+          let a = document.createElement('a');
+          a.href = `./cargas/${element[4]}`;
+          a.innerHTML = element[6];
+          a.target = "_blank";
+          a.style = "margin-right:10px";
+          div.appendChild(img);
+          //se mete el elemento "a" en el div
+          div.appendChild(a);
+          //-----------------
+          let td5 = document.createElement('td');
+          td5.innerHTML = element[6];
+          let td6 = document.createElement('td');
+          td6.innerHTML = element[5];
+          a.appendChild(td5);
+          //se meten todos los td en el elemento tr
+          tr.appendChild(td1);
+          tr.appendChild(td2);
+          tr.appendChild(td3);
+          tr.appendChild(td4);
+          tr.appendChild(td5);
+          tr.appendChild(td6);
+          //se agrega el elemento tr en la tabla
+          body_tabla.appendChild(tr);
+          //se agrega el div con los pdfs en donde va
+          docs.appendChild(div);
+        });
+
+        //obtener el credito, el cual se encuentra en la posicion 5 del arreglo "datos", despues sumo todos los creditos y los guardo en una variable para mostrarlos en pantalla --------------------------
+        const valor = document.getElementById("credito_valor");
+        let valorFinal = 0;
+        datos.forEach(element => {
+          //variable que convierte los creditos a numeros
+          let numero = Number(element[5]);
+          //se hace la suma de los creditos
+          valorFinal = valorFinal + numero;
+        });
+        //se crea un elemento b
+        let b = document.createElement('b');
+        //se le agrega al elemento creado la suma de los creditos
+        b.innerHTML = valorFinal;
+        valor.appendChild(b);
+        //console.log(b);
+        // -----------------------------
+
+      }
+
+
+    })
+
+
+  };
+</script>

4522332c3b4acb7d5b4eeea0678e93b4b59fb93f
AntonioQk <86135476+AntonioQk@users.noreply.github.com>
2022-10-22 19:02:49 -0500

    Add files via upload

---
+++ usuarios/constancia_datos.php
@@ -0,0 +1,45 @@
+<?php
+
+include('../app/config/config.php');
+
+if (isset($_POST['buscar'])) {
+  $matricula_buscar = $_POST['buscar_matricula'];
+  $datos = array();
+  //$datos['existe'] = "0";
+  /*$nombre = array();
+  $obs = array();
+  $desemp = array();
+  $valor = array();
+  $ruta = array();
+  $credito = array();
+  $title = array();
+*/
+
+  $resultados = mysqli_query($conexion, "SELECT nombre, observacion, desmp, valor, ruta_doc, credito, title FROM creditos INNER JOIN evidencia ON creditos.id_evento = evidencia.id_evento INNER JOIN events on creditos.id_evento = events.id WHERE creditos.matricula = '$matricula_buscar'");
+  while ($consulta = mysqli_fetch_array($resultados)) {
+    /*$nombre[] = $consulta['nombre'];
+    $obs[] = $consulta['observacion'];
+    $desemp[] = $consulta['desmp'];
+    $valor[] = $consulta['valor'];
+    $ruta[] = $consulta['ruta_doc'];
+    $credito[] = $consulta['credito'];
+    $title[] = $consulta['title'];*/
+    $datos[] = $consulta;
+  }
+
+  $datos = json_encode($datos);
+  echo $datos;
+  /*$datos['existe'] = "1";
+  $datos['nombre'] = $nombre;
+  $datos['obs'] = $obs;
+  $datos['desemp'] = $desemp;
+  $datos['valor'] = $valor;
+  $datos['ruta'] = $ruta;
+  $datos['credito'] = $credito;
+  $datos['title'] = $title;*/
+}
+
+
+
+
+mysqli_close($conexion);

2eeec0827bc22f994199c6ce83325a6a4313405c
AntonioQk <86135476+AntonioQk@users.noreply.github.com>
2022-10-30 16:47:39 -0600

    Update controlador.php

--- usuarios/controlador.php
+++ usuarios/controlador.php
@@ -44,12 +44,18 @@ $location = "update_usuarios/" . $filename;
 move_uploaded_file($_FILES['file']['tmp_name'], $location);
 //echo $nombres ." - ".$ap_paterno." - ".$ap_materno." - ".$sexo." - ".$numero_control." - ".$carrera." - ".$correo." - ".$estado_civil." - ".$telefono." - ".$ciudad." - ".$colonia." - ".$calle." - ".$codigo_postal." - ".$curp." - ".$fecha_nacimiento." - ".$nivel_escolar." - ".$reticula." - ".$entidad." - ".$contraseña." - ".$user_creacion. " - ".$fechaHora." - ".$estado;
 
-$inserta = "INSERT INTO tb_usuarios(nombres, ap_paterno, ap_materno, sexo, correo, telefono, cargo, profesion, cubiculo, area, foto_perfil, contraseña, user_creacion, fyh_creacion, estado) VALUES ('$nombres', '$ap_paterno', '$ap_materno', '$sexo', '$correo', '$telefono', '$cargo', '$profesion', '$cubiculo', '$area', '$filename', '$contraseña', '$user_creacion', '$fechaHora', '$estado')";
-
-$resultado = mysqli_query($conexion, $inserta);
-if (!$resultado) {
-  echo 'Error al registrarse';
+$existencia = mysqli_query($conexion, "SELECT correo FROM tb_usuarios where correo='$correo'");
+if ($existencia) {
+  echo '<script language="javascript">alert("El usuario ya existe");window.location.href="create_usuario.php"</script>';
+  echo $existencia;
 } else {
-  echo '<script language="javascript">alert("Usuario registrado");window.location.href="create_usuario.php"</script>';
+  $inserta = "INSERT INTO tb_usuarios(nombres, ap_paterno, ap_materno, sexo, correo, telefono, cargo, profesion, cubiculo, area, foto_perfil, contraseña, user_creacion, fyh_creacion, estado) VALUES ('$nombres', '$ap_paterno', '$ap_materno', '$sexo', '$correo', '$telefono', '$cargo', '$profesion', '$cubiculo', '$area', '$filename', '$contraseña', '$user_creacion', '$fechaHora', '$estado')";
+
+  $resultado = mysqli_query($conexion, $inserta);
+  if (!$resultado) {
+    echo '<script language="javascript">alert("No se pudo guardar. Inténtalo de nuevo.");window.location.href="create_usuario.php"</script>';
+  } else {
+    echo '<script language="javascript">alert("Usuario registrado");window.location.href="create_usuario.php"</script>';
+  }
 }
 mysqli_close($conexion);

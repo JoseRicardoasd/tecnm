171e91a6414187d64f3eaa9d1e38020662edd0a2
AntonioQk <86135476+AntonioQk@users.noreply.github.com>
2022-10-31 16:18:06 -0600

    Update calendariovista.php

--- usuarios/calendariovista.php
+++ usuarios/calendariovista.php
@@ -39,7 +39,20 @@ if (isset($_SESSION['u_usuario']) && $_SESSION['u_privilegio']  == 2 ) {
   $req->execute();
 
   $events = $req->fetchAll();
+  
+  //control de inactividad
+  $ahora = date("Y-n-j H:i:s");
+  $fechaGuardada = $_SESSION["ultimoAcceso"];
+  $tiempo_transcurrido = (strtotime($ahora) - strtotime($fechaGuardada));
 
+  if ($tiempo_transcurrido >= 600) {
+    //si pasaron 10 minutos o más
+    session_destroy(); // destruyo la sesión
+    header('location:../index.php'); //envío al usuario a la pag. de autenticación
+    //sino, actualizo la fecha de la sesión
+  } else {
+    $_SESSION["ultimoAcceso"] = $ahora;
+  }
 
 ?>
 
@@ -59,7 +72,10 @@ if (isset($_SESSION['u_usuario']) && $_SESSION['u_privilegio']  == 2 ) {
 
       <!-- Content Wrapper. Contains page content -->
       <div class="content-wrapper">
-        <!-- Content Header (Page header) -->
+        <!-- cierre sesion por inactividad -->
+        <?php if ($_SESSION["ultimoAcceso"] >= 600) {
+          echo ("<meta http-equiv='refresh' content='600'>");
+        } ?>
         <section class="content-header">
           <h1>
             SISTEMA DE CREDITOS COMPLENTARIOS

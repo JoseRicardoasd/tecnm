75933a9e670b26cf330f2fe36b80a74b6d8b6603
DianaMB06 <105074653+DianaMB06@users.noreply.github.com>
2022-11-01 20:06:34 -0600

    Update footer.php

--- layout/footer.php
+++ layout/footer.php
@@ -2,8 +2,8 @@
 ?>
 <footer class="main-footer">
     <div class="pull-right hidden-xs">
-      <b>Version</b> 1.0.0
+      <b>Versión</b> 2.0.0
     </div>
-    <strong>Copyright &copy; 2020 <a href="www.facebook.com">Sistema de Creditos Complementarios</a>.</strong> All rights
+    <strong>Copyright &copy; 2020 <a href="www.facebook.com">Créditos de Actividades Complementarios</a>.</strong> All rights
     reserved.
   </footer>

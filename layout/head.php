9305ad8fce7245a69bd58cd263e2815ede4c33b6
JoseRicardoasd <105124201+JoseRicardoasd@users.noreply.github.com>
2022-11-08 21:13:39 -0600

    Update head.php

--- layout/head.php
+++ layout/head.php
@@ -7,6 +7,8 @@
 <!-- Tell the browser to be responsive to screen width -->
 <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
 <!-- Bootstrap 3.3.6 -->
+
+<link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
 <link rel="stylesheet" href="../app/templeates/AdminLTE-2.3.11/bootstrap/css/bootstrap.css">
 <!-- Font Awesome -->
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.5.0/css/font-awesome.min.css">

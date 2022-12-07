4786055f333e432353c2f8dc6690eba551fc7b13
JoseRicardoasd <105124201+JoseRicardoasd@users.noreply.github.com>
2022-12-07 13:14:31 -0600

    Update menu.php

--- layout/menu.php
+++ layout/menu.php
@@ -2,9 +2,9 @@
   <!-- Logo -->
   <a href="index.php" class="logo">
     <!-- mini logo for sidebar mini 50x50 pixels -->
-    <span class="logo-mini"><b>TECNM</b></span>
+    <span class="logo-mini"><b>Chiná</b></span>
     <!-- logo for regular state and mobile devices -->
-    <span class="logo-lg"><b>TecNM</b>Chiná</span>
+    <span class="logo-lg"><b>Chiná</b>TecNM</span>
   </a>
   <!-- Header Navbar: style can be found in header.less -->
   <nav class="navbar navbar-static-top">

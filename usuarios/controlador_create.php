ea98d1df4c086f6cf099c4b5ba15cee132eec7e4
AntonioQk <86135476+AntonioQk@users.noreply.github.com>
2022-12-08 01:29:59 -0600

    Update controlador_create.php

--- usuarios/controlador_create.php
+++ usuarios/controlador_create.php
@@ -31,35 +31,35 @@ $user_creacion = "ESCAMILLA";
 
 if ($contraseña == $contraseñaConfirm) {
 
-//encriptar contraseña
-$contraseña = password_hash($contraseña, PASSWORD_DEFAULT, ['cost' => 10]);
+  //encriptar contraseña
+  $contraseña = password_hash($contraseña, PASSWORD_DEFAULT, ['cost' => 10]);
 
-date_default_timezone_set("America/Monterrey");
+  date_default_timezone_set("America/Monterrey");
 
-$fechaHora = date('Y-m-d h:i:s');
-$estado = '1';
+  $fechaHora = date('Y-m-d h:i:s');
+  $estado = '1';
 
-$nombre_de_foto_perfil = "SisTECNM-" . date('Y-m-d-h-i-s');
-$filename = $nombre_de_foto_perfil . "_" . $_FILES['file']['name'];
+  $nombre_de_foto_perfil = "SisTECNM-" . date('Y-m-d-h-i-s');
+  $filename = $nombre_de_foto_perfil . "_" . $_FILES['file']['name'];
 
-$location = "update_usuarios/" . $filename;
+  $location = "update_usuarios/" . $filename;
 
-move_uploaded_file($_FILES['file']['tmp_name'], $location);
-//echo $nombres ." - ".$ap_paterno." - ".$ap_materno." - ".$sexo." - ".$numero_control." - ".$carrera." - ".$correo." - ".$estado_civil." - ".$telefono." - ".$ciudad." - ".$colonia." - ".$calle." - ".$codigo_postal." - ".$curp." - ".$fecha_nacimiento." - ".$nivel_escolar." - ".$reticula." - ".$entidad." - ".$contraseña." - ".$user_creacion. " - ".$fechaHora." - ".$estado;
+  move_uploaded_file($_FILES['file']['tmp_name'], $location);
+  //echo $nombres ." - ".$ap_paterno." - ".$ap_materno." - ".$sexo." - ".$numero_control." - ".$carrera." - ".$correo." - ".$estado_civil." - ".$telefono." - ".$ciudad." - ".$colonia." - ".$calle." - ".$codigo_postal." - ".$curp." - ".$fecha_nacimiento." - ".$nivel_escolar." - ".$reticula." - ".$entidad." - ".$contraseña." - ".$user_creacion. " - ".$fechaHora." - ".$estado;
 
-$inserta = "INSERT INTO tb_usuarios (nombres, ap_paterno, ap_materno, sexo, numero_control, carrera, correo, estado_civil, telefono, ciudad, colonia, calle, codigo_postal, curp, fecha_nacimiento, foto_perfil, contrasenia, cargo, user_creacion, fyh_creacion, estado) VALUES ('$nombres', '$ap_paterno', '$ap_materno', '$sexo', '$numero_control', '$carrera', '$correo', '$estado_civil', '$telefono', '$ciudad', '$colonia', '$calle', '$codigo_postal', '$curp', '$fecha_nacimiento', '$filename', '$contraseña', 2, '$user_creacion', '$fechaHora', '$estado')";
+  $inserta = "INSERT INTO tb_usuarios (nombres, ap_paterno, ap_materno, sexo, numero_control, carrera, correo, estado_civil, telefono, ciudad, colonia, calle, codigo_postal, curp, fecha_nacimiento, foto_perfil, contrasenia, cargo, user_creacion, fyh_creacion, estado) VALUES ('$nombres', '$ap_paterno', '$ap_materno', '$sexo', '$numero_control', '$carrera', '$correo', '$estado_civil', '$telefono', '$ciudad', '$colonia', '$calle', '$codigo_postal', '$curp', '$fecha_nacimiento', '$filename', '$contraseña', 2, '$user_creacion', '$fechaHora', '$estado')";
 
-$resultado = mysqli_query($conexion, $inserta);
-if (!$resultado) {
-  if (mysqli_error($conexion) == "Duplicate entry '$correo' for key 'tb_usuarios.PRIMARY'") {
-    echo '<script language="javascript">alert("El usuario ya existe");window.location.href="create.php"</script>';
+  $resultado = mysqli_query($conexion, $inserta);
+  if (!$resultado) {
+    if (mysqli_error($conexion) == "Duplicate entry '$correo' for key 'tb_usuarios.PRIMARY'") {
+      echo '<script language="javascript">alert("El usuario ya existe");window.location.href="create.php"</script>';
+    } else {
+      echo '<script language="javascript">alert("No se pudo guardar. Inténtalo de nuevo.");window.location.href="create.php"</script>';
+    }
   } else {
-    echo '<script language="javascript">alert("No se pudo guardar. Inténtalo de nuevo.");window.location.href="create.php"</script>';
-  }
-} else {
 
-  echo '<script language="javascript">alert("Usuario registrado");window.location.href="create.php"</script>';
-}
+    echo '<script language="javascript">alert("Usuario registrado");window.location.href="create.php"</script>';
+  }
 
 } else {
   echo '<script language="javascript">alert("Las contraseñas no coinciden");window.location.href="create.php"</script>';

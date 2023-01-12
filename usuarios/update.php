a02f5064e1c3457be21cc777fb9d363b0f3f6cc2
carlos <carlos@gmail.com>
2023-01-12 00:49:18 -0600

    cambios extra

--- usuarios/update.php
+++ usuarios/update.php
@@ -8,7 +8,6 @@ $idRegistros = $_REQUEST['id'];
 $control = $_REQUEST['control'];
 $actividad = $_REQUEST['actividad'];
 $habilidad = $_REQUEST['habilidad'];
-$calificacion = $_REQUEST['calificacion'];
 $tiempo = $_POST['tiempo'];
 $equipo = $_POST['equipo'];
 $liderazgo = $_POST['liderazgo'];
@@ -20,18 +19,23 @@ $iniciativa = $_POST['iniciativa'];
 $resultado = ($tiempo + $equipo + $liderazgo + $organiza + $interpreta + $realiza + $iniciativa) / 7;
 if ($resultado <= 4) {
   $valor = 'Excelente';
+  $calificacion = 'Acreditado';
 }
 if ($resultado <= 3) {
   $valor = 'Notable';
+  $calificacion = 'Acreditado';
 }
 if ($resultado <= 2) {
   $valor = 'Bueno';
+  $calificacion = 'Acreditado';
 }
 if ($resultado <= 1) {
   $valor = 'Suficiente';
+  $calificacion = 'NO Acreditado';
 }
 if ($resultado <= 0) {
   $valor = 'Insuficiente';
+  $calificacion = 'NO Acreditado';
 };
 
 $update = ("UPDATE extragrupo 

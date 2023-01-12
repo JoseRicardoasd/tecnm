0783ba7eae6f177bc77cb40473b5f9051793cdcc
juan cu <Antony556_04@outlook.com>
2023-01-12 00:30:11 -0600

    msj confirmacion en foto de perfil

--- layout/menu.php
+++ layout/menu.php
@@ -72,7 +72,7 @@
                   if ($buscar == true) {
                     // echo "existe foto de perfil";
                   ?>
-                    <img src="../usuarios/update_usuarios/<?php echo $sesion_usuario['foto_perfil']; ?>" class="user-image" alt="User Image">
+                    <img src="data:imagen/png;base64,<?php echo base64_encode($sesion_usuario['foto']) ?>" class="user-image" alt="User Image">
                     <?php
                   } else {
                     if ($id_sexo  == "Hombre") {
@@ -246,24 +246,24 @@
       </li>
 
       <li class="treeview">
-          <a href="#">
-            <i class="fa fa-users"></i> <span>Configuración</span>
-            <span class="pull-right-container">
-              <i class="fa fa-angle-left pull-right"></i>
-            </span>
-          </a>
-          <ul class="treeview-menu">
+        <a href="#">
+          <i class="fa fa-users"></i> <span>Configuración</span>
+          <span class="pull-right-container">
+            <i class="fa fa-angle-left pull-right"></i>
+          </span>
+        </a>
+        <ul class="treeview-menu">
 
-            <li><a href="lista-jefes.php"><i class="fa fa-users"></i>Lista de Jefes</a></li>
+          <li><a href="lista-jefes.php"><i class="fa fa-users"></i>Lista de Jefes</a></li>
 
-            <li><a href="create_jefes.php"><i class="fa fa-user"></i> Agregar Jefes de departamento</a></li>
+          <li><a href="create_jefes.php"><i class="fa fa-user"></i> Agregar Jefes de departamento</a></li>
 
-            <li><a href="añadirDepartamento.php"><i class="fa fa-users"></i>Departamentos</a></li>
+          <li><a href="añadirDepartamento.php"><i class="fa fa-users"></i>Departamentos</a></li>
 
-            <li><a href="formato_constancia.php"><i class="fa fa-book"></i> <span><b>Formato de la constancia</b></span></a></li>
+          <li><a href="formato_constancia.php"><i class="fa fa-book"></i> <span><b>Formato de la constancia</b></span></a></li>
 
-          </ul>
-        </li>
+        </ul>
+      </li>
 
 
       <!-- creditos -->

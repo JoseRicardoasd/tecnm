a34b9a734a13fb82b0e793c25eb52b62b8e0b1b0
carlos <carlos@gmail.com>
2023-01-12 01:01:38 -0600

    cambios nuevo

--- usuarios/update.php
+++ usuarios/update.php
@@ -3,7 +3,7 @@
 include('../app/config/config.php');
 // obtenemos el valor y el resultado
 $resultado = 0;
-$valor = null;
+$valor = '';
 $idRegistros = $_REQUEST['id'];
 $control = $_REQUEST['control'];
 $actividad = $_REQUEST['actividad'];
@@ -20,25 +20,7 @@ $resultado = ($tiempo + $equipo + $liderazgo + $organiza + $interpreta + $realiz
 if ($resultado <= 4) {
   $valor = 'Excelente';
   $calificacion = 1;
-}
-if ($resultado <= 3) {
-  $valor = 'Notable';
-  $calificacion = 1;
-}
-if ($resultado <= 2) {
-  $valor = 'Bueno';
-  $calificacion = 1;
-}
-if ($resultado <= 1) {
-  $valor = 'Suficiente';
-  $calificacion = 2;
-}
-if ($resultado <= 0) {
-  $valor = 'Insuficiente';
-  $calificacion = 2;
-};
-
-$update = ("UPDATE extragrupo 
+  $update = ("UPDATE extragrupo 
 	SET 
 	observacion  ='" .$habilidad. "',
 	valor  ='" .$resultado. "',
@@ -50,5 +32,70 @@ WHERE matricula='" .$control. "' AND idActividad='".$actividad."'
 $result_update = mysqli_query($por, $update);
 
 header('Location: alumnosListado.php?id='.$actividad);
+}
+if ($resultado <= 3) {
+  $valor = 'Notable';
+  $calificacion = 1;
+  $update = ("UPDATE extragrupo 
+	SET 
+	observacion  ='" .$habilidad. "',
+	valor  ='" .$resultado. "',
+	desempeño  ='" .$valor. "',
+	acreditacion  ='" .$calificacion. "'
+
+WHERE matricula='" .$control. "' AND idActividad='".$actividad."'
+");
+$result_update = mysqli_query($por, $update);
+
+header('Location: alumnosListado.php?id='.$actividad);
+}
+if ($resultado <= 2) {
+  $valor = 'Bueno';
+  $calificacion = 1;
+  $update = ("UPDATE extragrupo 
+	SET 
+	observacion  ='" .$habilidad. "',
+	valor  ='" .$resultado. "',
+	desempeño  ='" .$valor. "',
+	acreditacion  ='" .$calificacion. "'
+
+WHERE matricula='" .$control. "' AND idActividad='".$actividad."'
+");
+$result_update = mysqli_query($por, $update);
+
+header('Location: alumnosListado.php?id='.$actividad);
+}
+if ($resultado <= 1) {
+  $valor = 'Suficiente';
+  $calificacion = 2;
+  $update = ("UPDATE extragrupo 
+	SET 
+	observacion  ='" .$habilidad. "',
+	valor  ='" .$resultado. "',
+	desempeño  ='" .$valor. "',
+	acreditacion  ='" .$calificacion. "'
+
+WHERE matricula='" .$control. "' AND idActividad='".$actividad."'
+");
+$result_update = mysqli_query($por, $update);
+
+header('Location: alumnosListado.php?id='.$actividad);
+}
+if ($resultado <= 0) {
+  $valor = 'Insuficiente';
+  $calificacion = 2;
+  $update = ("UPDATE extragrupo 
+	SET 
+	observacion  ='" .$habilidad. "',
+	valor  ='" .$resultado. "',
+	desempeño  ='" .$valor. "',
+	acreditacion  ='" .$calificacion. "'
+
+WHERE matricula='" .$control. "' AND idActividad='".$actividad."'
+");
+$result_update = mysqli_query($por, $update);
+
+header('Location: alumnosListado.php?id='.$actividad);
+};
 
 ?>

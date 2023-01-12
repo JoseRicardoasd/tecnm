600e74b7e1a044b1a8dcb7befa0add3aaa6a46d9
BOTON999 <114854537+B0T0N@users.noreply.github.com>
2023-01-12 00:40:09 -0600

    Update alumnosListado.php

--- usuarios/php/extra/alumnosListado.php
+++ usuarios/php/extra/alumnosListado.php
@@ -12,7 +12,7 @@ $extraescolar = $query->fetch(PDO::FETCH_LAZY);
 
 $nombre_actividad = $extraescolar['nombreActividad'];
 
-$sqlCliente   = ("SELECT tb_usuarios.id, tb_usuarios.nombres,tb_usuarios.ap_paterno,tb_usuarios.ap_materno,tb_usuarios.carrera,tb_usuarios.numero_control,tb_usuarios.telefono,extragrupo.observacion,extragrupo.desempeño,extragrupo.acreditacion,extragrupo.idActividad FROM extragrupo INNER JOIN tb_usuarios ON extragrupo.matricula = tb_usuarios.numero_control WHERE extragrupo.idActividad = $id");
+$sqlCliente   = ("SELECT tb_usuarios.id, tb_usuarios.nombres,tb_usuarios.ap_paterno,tb_usuarios.ap_materno,tb_usuarios.carrera,tb_usuarios.numero_control,tb_usuarios.telefono,extragrupo.observacion,extragrupo.desempeyo,extragrupo.acreditacion,extragrupo.idActividad FROM extragrupo INNER JOIN tb_usuarios ON extragrupo.matricula = tb_usuarios.numero_control WHERE extragrupo.idActividad = $id");
 $queryCliente = mysqli_query($conexion, $sqlCliente);
 $cantidad     = mysqli_num_rows($queryCliente);
 

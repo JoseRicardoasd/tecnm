8a5a5bbcd61c315d73e03fcf891cdd05b002ca03
juan cu <Antony556_04@outlook.com>
2023-01-17 13:29:11 -0600

    cerrar session en editar evento y eliminar

--- usuarios/editar.php
+++ usuarios/editar.php
@@ -5,10 +5,10 @@ include('../app/config/config.php');
 
 $id = $_POST['id_editar'];
 
-$actividad=$_POST['actividad'];
-$descripcion=$_POST['descripcion'];
-$credito=$_POST['credito'];
-$maximo=$_POST['maximo'];
+$actividad = $_POST['actividad'];
+$descripcion = $_POST['descripcion'];
+$credito = $_POST['credito'];
+$maximo = $_POST['maximo'];
 
 $sql = "UPDATE guia SET actividad = '$actividad' ,descripcion = '$descripcion',
 credito = '$credito' , maximo ='$maximo' WHERE id = $id";
@@ -19,15 +19,17 @@ credito = '$credito' , maximo ='$maximo' WHERE id = $id";
 
 echo $sql;
 
-    $query = $bdd->prepare($sql);
-    if ($query == false) {
-        print_r($bdd->errorInfo());
-        die('Erreur prepare');
-    }
-    $sth = $query->execute();
-    if ($sth == false) {
-        print_r($query->errorInfo());
-        die('Erreur execute');
-    }
-    header('Location: guia.php');
-?>
+$query = $bdd->prepare($sql);
+if ($query == false) {
+  print_r($bdd->errorInfo());
+  die('Erreur prepare');
+}
+$sth = $query->execute();
+if ($sth == false) {
+  print_r($query->errorInfo());
+  die('Erreur execute');
+}
+header('Location: guia.php');
+
+
+mysqli_close($conexion);

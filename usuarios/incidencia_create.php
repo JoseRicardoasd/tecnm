029d6b467e214b4ee8acde84ffdd2bd874c5b811
Rick <rickchanmariin17@gmail.com>
2023-01-23 13:07:17 -0600

    Update incidencias_create.php

--- usuarios/incidencia_create.php
+++ usuarios/incidencia_create.php
@@ -1,11 +1,5 @@
 <?php
-require 'phpmailer/PHPMailer.php';
-require 'phpmailer/Exception.php';
-require 'phpmailer/SMTP.php';
 
-use PHPMailer\PHPMailer\PHPMailer;
-use PHPMailer\PHPMailer\SMTP;
-use PHPMailer\PHPMailer\Exception;
 
 include ('../app/config/config.php');
 
@@ -19,35 +13,8 @@ if (isset($_POST['motivo']) && isset($_POST['categoria']) && isset($_POST['prior
 	$incidencia = $_POST['incidencia'];
     $actualiza = $_POST['motivoac'];
 
-$mail = new PHPMailer(true);
 
-try{
-    $mail ->SMTPDebug = SMTP::DEBUG_SERVER;
-    $mail ->isSMTP();
-    $mail->Host = 'smtp.gmail.com';
-    $mail->SMTPAuth = true;
-    $mail->Username = 'fxrfxn4@gmail.com';
-    $mail->Password = 'syacuhqgbmqcccnr';
-    $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
-    $mail->Port = 587;
-
-//
-    $mail->setFrom ('fxrfxn4@gmail.com','INSTITUTO TECNOLOGICO DE CHINA');
-    $mail->addAddress('danielfarfantecnm@gmail.com');
-
-
-    $mail->isHTML(true);
-    $mail->Subject = 'Prueba de registro de incidencia';
-    $mail->Body = $motivo.$categoria.$id_alumno;
-    $mail->send();
-
-    echo 'Correo enviado con exito';
-}catch(Exception $e){
-    echo 'El mensaje no se ha enviado'.$mail->ErrorInfo;
-}
-
-
- $sql= "INSERT INTO tb_incidencia(motivo,categoria,prioridad,id_alumno,Estado,motivo_actualizacion) VALUES ('$motivo','$categoria','$prioridad','$id_alumno','$incidencia','$actualiza')";
+ $sql= "INSERT INTO tb_incidencia(id_alumno,motivo,categoria,prioridad,Estado,motivo_actualizacion) VALUES ('$id_alumno','$motivo','$categoria','$prioridad','$incidencia','$actualiza')";
 
 	
 	echo $sql;
